3a18eaf3a6dc2df6f1a3970f4faf981199f68883
Jorge Meneses <jorge.meneses@ucr.ac.cr>
2023-12-03 02:04:04 -0600

    Funcion distancia entre puntos y ley de cosenos

--- proyecto2.c
+++ proyecto2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+double PI = 3.14159265358979323846;
+
+float pointDist(float xA, float yA, float xB, float yB){
+    float dist = sqrt(pow(xB-xA, 2) + pow(yB-yA, 2));
+    return dist;
+}
+
+float cosLaw(float a, float b, float c){
+    float angle = acos((pow(c, 2) - pow(a, 2) - pow(b, 2))/(-2*a*b));
+    return (angle*180)/PI;
+}
+
+int main(){
+    return 0;
+}
