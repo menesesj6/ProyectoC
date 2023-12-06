a482261c83824bd361dc686a499f1ed42c74589f
Jorge Meneses <jorge.meneses@ucr.ac.cr>
2023-12-06 15:16:35 -0600

    Problema 1 totalmente funcional

--- proyecto1.c
+++ proyecto1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
 
 void assign(int* a, int* b){
+    // Asigna valores, usado para reflejar distancias en matriz de distancias
     *b = *a;
 }
 
@@ -14,39 +14,41 @@ void swap(int *a, int *b){
 }
 
 int fact(int x){
+    // Calcula el factorial de un numero x, usado para el tamano de la matriz de posibles ordenes
     if(x == 0){
         return 1;
     } else{
-        return x*fact(x-1);
+        return x*fact(x-1); // Recursion
     }
 }
 
 int perms(int cityIndexes[], int N, int index, int auxArray[], int * count){
-    int i, j;
+    // EXplora todas las combinaciones posibles del array de ciudades
+    int i;
     if (index == N-1){
         for (i = 0; i < N; i++){
-        assign(&cityIndexes[i], &auxArray[i + (*count)]);
+        assign(&cityIndexes[i], &auxArray[i + (*count)]); // Se anade la combinacion en un array auxiliar
         }
         (*count) += N;
     }
     for (i = index; i < N; i++){
         swap(&cityIndexes[index], &cityIndexes[i]);
-        perms(cityIndexes, N, index + 1, auxArray, count);
+        perms(cityIndexes, N, index + 1, auxArray, count); // Recursion
         swap(&cityIndexes[index], &cityIndexes[i]);
     }
 }
 
 int main() {
     int N; // Cantidad de ciudades total
-    int i, j, k, m, l; // Para iteraciones
+    int i, j, k, l; // Para iteraciones
     int num; // Valor ingresado por usuario
     int originIndex, destinyIndex; // Indice del destino y el origen
     int distToBeat; // DIstancia directa, se debe ser menor a eso
-    int sum = 0;
-    int count = 0;
+    int count = 0; // COntador, usado para extraer las posibilidades acomodo de ciudades en un array auxiliar
+    int distTOTAL; // Distancia de un camino recorrido
     char destiny, origin; // Punto inicial y final
     printf("Ingrese la cantidad de ciudades: \n");
-    scanf("%d", &N);
+    scanf("%d", &N); // Se obtiene cantidad de ciudades
     int dists[N][N]; // Matriz de distancias
     int bestPath[N];
     // Asignar la diagonal de 0s
@@ -95,21 +97,21 @@ int main() {
     printf("Ingrese su ciudad de destino: \n "); // Obtener la ciudad de destino
     scanf(" %c", &destiny);
 
-    int cityIndexes[N]; // Array de posibles ciudades por letra
+    int cityIndexes[N]; // Array de indices de las posibles ciudades
     for (i = 0; i < N; i++){
         cityIndexes[i] = i;
     }
     
-    char cities[N]; // Ciudades en chars
+    char cities[N]; // Array de las posibles ciudades
     for (i = 0; i < N; i++){
         cities[i] = 'A' + i;
     }
 
-    originIndex = origin - 'A'; // Indice numerico de la ciudad de origen
+    originIndex = origin - 'A'; // Indice numerico de la ciudad de origen usando ASCII
     if (originIndex > cityIndexes[N-1]){
         printf("ERROR, ciudad origen fuera de rango.");
     }
-    destinyIndex = destiny - 'A'; // Indice numerico de la ciudad destino
+    destinyIndex = destiny - 'A'; // Indice numerico de la ciudad destino usando ASCII
     if (destinyIndex > cityIndexes[N-1]){
         printf("ERROR, ciudad destino fuera de rango.");
     }
@@ -124,12 +126,15 @@ int main() {
         printf("\nNo hay camino directo entre %c y %c. Buscando otro...", originIndex + 'A', destinyIndex + 'A');
         distToBeat = __INT_MAX__; // Macro maximo numerico para buscar algo menor
     }
+
     // Logica de camino mas corto
-    int auxArray[fact(N)*N]; // Array con caminos posibles
+
+    int auxArray[fact(N)*N]; // Array auxiliar con caminos posibles
     int Paths[fact(N)][N]; // Matriz de caminos posibles
-    perms(cityIndexes, N, 0, auxArray,  &count); // Iterar osbre todos los caminos posibles
+
+    perms(cityIndexes, N, 0, auxArray,  &count); // Iterar sobre todos los caminos posibles
     
-    // Asignar la matriz de caminos posibles
+    // Pasar de array auxiliar a matriz de caminos posibles
     for (i = 0; i < fact(N); i++){
         for (j = 0; j < N; j++){
             Paths[i][j] = auxArray[k];
@@ -137,29 +142,27 @@ int main() {
         }
     }
 
-    int distTOTAL;
+    // Iterar sobre los caminos posibles
     for (i = 0; i < fact(N); i++){
         if (Paths[i][0] != originIndex){
-            continue;
+            continue; // Descartar el camino si no empieza en el origen
         } else{
-            distTOTAL = 0;
-            for (m = 0; m < N; m++){
-            }
+            distTOTAL = 0; // Resetear el la distancia total del camino a iterar
             for (j = 0; j < N; j++){
             if (Paths[i][j] == destinyIndex){
-                distTOTAL = distTOTAL;
-                break;
+                distTOTAL = distTOTAL; // Mantener el valor de distancia al ya llegar al destino
+                break; // Quebrar el ciclo porque se llego al destino
             } else if (dists[Paths[i][j]][Paths[i][j+1]] == -1){
-                distTOTAL = __INT_MAX__;
-                break;
+                distTOTAL = __INT_MAX__; // Disparar el valor de distancia cuando -1, para que este caso nunca sea menor a la distancia a vencer
+                break; // Quebrar el ciclo por no conexion
             } else{
-                distTOTAL += dists[Paths[i][j]][Paths[i][j+1]];
+                distTOTAL += dists[Paths[i][j]][Paths[i][j+1]]; // Sumar todas las distancias pasadas por el momento
                 } // FIn iteracion camino
             }
-            if(distTOTAL < distToBeat && distTOTAL > 0){
+            if(distTOTAL < distToBeat && distTOTAL > 0){ // Revisar si la distancia del camino e sla mas corta hasta ahora
                 distToBeat = distTOTAL;
                 for(l = 0; l < N; l++){
-                    bestPath[l] = Paths[i][l];
+                    bestPath[l] = Paths[i][l]; // Asignar el array de mejor camino, por le momento 
                 }
             }
 
@@ -167,7 +170,7 @@ int main() {
         
         }
     printf("\n\nENCONTRADO! Camino mas corto:\n");
-
+    // Imprimir resultados
     for (i = 0; i < N && bestPath[i] != destinyIndex; i++){
         printf("-> %c ", cities[bestPath[i]]);
     }
