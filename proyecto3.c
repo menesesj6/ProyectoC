2fb56a98c8e445a40dc828989cdd2ccc5149e767
KristhelQuesada <kristhelquesada24@gmail.com>
2023-12-06 18:51:37 -0600

    Programa listo, con sanitys completos, revisa que la cantidad ingresada de valores concuerde con la escogida, y que los numeros de N y M no sean negativos, ni sean de otro tipo que no sea entero.

--- proyecto3.c
+++ proyecto3.c
@@ -6,36 +6,37 @@ Código realizado por:
 Descripción:
 El siguiente código permite que el usuario cree una matriz de
 NxM donde los datos almacenados serian de manera dinamica. En
-el programa se hara uso de una funcion que recibe una cantidad
-arbitrario de numeros enteros que representen las posiciones
-dentro de la matriz. Asimismo, se realizan sanity checks tal
-que si la funcion retorna un 0 entonces no hubo ningun error
-mas si retorna un -1 entonces se detecto un error, como que
-los valores ingresados no corresponden a un unico tipo, o los
-valores ingresados no son enteros.
-*/
-
-/*
-Se decide cambiar la logica a una representaci[on de matriz mas detallada,
-porque una matriz aca en C debe verse como un puntero de punteros, la idea
-anterior tenia un fallo en la logica porque cuando uno accede al puntero
-de un array, este tiene un unico puntero que esta asociado al primer byte
-del arreglo. Entonces, se intentara construir la matriz tal que se repre-
-sente como un array de arrays para mayor robustez.
-
-Entonces, vamos a tener un puntero, que apunta a la matriz y dentro de
-ella es donde vamos a proceder a obtener cada puntero correspondiente
-a las filas.
-
-Para se va a definir como: int **MATRIZ
-donde (*MATRIZ) se refiere a la matriz e (int *) se refiere a lo que con-
-tiene *MATRIZ, es decir, le indicamos que contiene punteros.
+el programa se hara uso de 4 funciones:
+1. La primera crea una matriz mediante memoria dinamica mediante
+una logica de que esta represente un puntero de punteros, ya que
+eso hace de manera mas sencilla la extraccion de los punteros de
+cada fila, aparte que le da mas robustez al concepto de matriz.
+2. La segunda funciona para darle estructura a la matriz y poderla
+visualizar de mejor manera.
+3. La tercera es una funcion que me permite crear una cantidad
+arbitraria de numeros empleando la menoria dinamica y el uso de
+reallocate para dimensionar la entrada ingresada segun los valo-
+res que disponga el usuario.
+4. La cuarta funcion es la funcion objetivo, la cual cumple con
+los requisitos que demanda el enunciado. Pasa por parametro la
+cantidad indefinida de numeros, los convierte a enteros, llena
+la matriz creada con los valores enteros y luego de tener la
+matriz creada completamente, extraemos los punteros que corres-
+ponden al valor de la primera fila. Luego, como segunda compro-
+bacion se despliega la matriz creada a como el usuario dispuso.
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// FUERADERANGO para detectar el error cuando se ingresa
+// una cantidad de valores diferente al tamanio asignado
+// a la matriz.
+enum ERRORES {
+    EXITO = 0,
+    FUERADERANGO = -1,
+};
 
 // Funcion para inicializar la matriz, y crear todas sus caracteristicas
 int **createMatrix(int N, int M) {
@@ -77,16 +78,28 @@ void viewMatrix(int **matriz, int N, int M) {
 
 
 // Funcion que permite crear una cantidad arbitraria de numeros enteros
+/*
+str es donde se almacenaran todos los valores que vayamos agregando, o que
+agrega el usuario, se define como memoria dinamica con malloc y se le asigna
+1*size creando primero un unico espacio en memoria de tamanio char. Luego una
+variable para el while, que define la posicion inicial del str.
+El while se va a encargar que cada que se ingrese una variable mediante el
+uso de getc que me permite recibir una entrada por parte del usuario o del
+standard input y siempre que sea distinto de un cambio de linea para que se
+guarden los cambios hasta donde se haya realizado. Luego para la posicion
+de str i se guarda ahi el valor ingresado. Finalmente, como todo string, fina-
+lizamos su ultimo valor con \0 y retornamos el string creado dinamicamente.
+*/
 char *dynamicString() {
-    char *str, c;
+    char *str, valor;
     int i = 0;
     str = (char*)malloc(1*sizeof(char));
 
     // PEDIRLE LOS VALORES AL USUARIO
     printf("Ingrese los numeros que desea almacenar en la matriz: \n");
 
-    while(c = getc(stdin), c != '\n') {
-        str[i] = c;
+    while(valor = getc(stdin), valor != '\n') {
+        str[i] = valor;
         i++;
         str = realloc(str, i*sizeof(char));
     }
@@ -99,28 +112,68 @@ char *dynamicString() {
 int funcionObjetivo(int **matriz, int rows, int columns, char *entrada) {
     // Inicializamos las variables
     char* cursor = entrada;
-    int i=0, j=0;
+    int i=0, j=0, contador=0;
+    int tamanio = rows*columns;
+    int status = EXITO;
+
+    if (rows<0 || columns<0){
+        printf("Ni filas y las columnas no pueden ser negativas\n");
+        status = FUERADERANGO; // Valor inválido, error
+        goto exit;
+    }
 
 
     // Aca hacemos casting de str a int para almacenarlas en la matriz
-
+    /*
+    Cursor es una variable que me permite recorrer el string.
+    De la libreria string.h se utiliza strlen y strtol. El primero permite
+    btener el tamanio de un string contando todo como un caracter de uno a
+    uno. El segundo permite convertir un string basicamente a un long int.
+    Para ello se le especifica en su parametro primero el valor que almacena
+    cursor (lo que va leyendo), luego su posicion en memoria y por ultimo
+    se especifica la base del numero, en nuestro caso decimal.
+    */
     while (cursor != entrada + strlen(entrada)) {
         long int x = strtol(cursor, &cursor, 10);
+
+        // Funciona para indicar el separador, en este caso
+        // se disenia para que se puedan separar los numeros
+        // por coma y por espacio. Si el cursor tiene como
+        // valor estos elementos entonces corre el puntero sin
+        // mas.
         while (*cursor == ' ' || *cursor == ',') {
             cursor++;
         }
 
+        // Para deteccion de errores
+        contador++;
+
+        if (tamanio < contador) {
+            printf(
+            "La cantidad de valores ingresados debe ser menor o igual que NxM o verifique que solo haya ingresado enteros\n"
+            );
+            status = FUERADERANGO;
+            goto exit;
+        }
+        
+        // como la matriz debe funcionar para enteros hacemos un casting
+        // de long int a int, y almacenamos el valor que registra el puntero
+        // en la posicion de la matriz
         int intval = (int) x;
         matriz[i][j] = intval;
 
+
+        // esto me permite controlar el flujo de almacenamiento, los valores
+        // se agregan de por columna y se cambia de fila una vez hayamos lle-
+        // gado al final de las columnas totales.
         if (j == columns-1) {
-            j = -1;
+            j = -1; // se setea en -1 ya que de otra forma no ajustaba
             i++;
         }
 
+        // esto es lo que permite ir aumentando la columna
         j=j+1;
     }
-    printf("\n");
 
 
     // Aca obtenemos los punteros asociados a cada fila y comprobamos
@@ -135,7 +188,8 @@ int funcionObjetivo(int **matriz, int rows, int columns, char *entrada) {
     printf("\nSu matriz creada es:\n");
     viewMatrix(matriz, rows, columns);
 
-    return 0;
+exit:
+    return status;
 }
 
 
